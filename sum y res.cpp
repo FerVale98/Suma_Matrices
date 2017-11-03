dcdaec2ad84a6d1dfb077380242174e4ee421998
Fernando Acevedo <32655586+FerVale98@users.noreply.github.com>
2017-11-02 19:03:59 -0600

    Add files via upload

---
+++ sum y res.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	int a,b,valor,valor2;
+	srand(time(NULL));
+	printf("Tamaño de la matriz:\n");
+	printf("Columnas: ");
+	scanf("%i",&a);
+	printf("\nFilas: ");
+	scanf("%i",&b);
+	int mat[a][b],mat2[a][b],suma[a][b],resta[a][b];
+	printf("\nMatriz 1:\n");
+	for(int i=0;i<a;i++){
+		 for(int j=0;j<b;j++){
+		 	valor=1+rand()%30;
+		 	mat[i][j]=valor;
+		 	printf("%i ",mat[i][j]);
+		 }
+		 printf("\n");
+	}
+	printf("\nMatriz 2:\n");
+	for(int i=0;i<a;i++){
+		 for(int j=0;j<b;j++){
+		 	valor2=1+rand()%30;
+		 	mat2[i][j]=valor2;
+		 	printf("%i ",mat2[i][j]);
+		 }
+		 printf("\n");
+	}
+	printf("\nSuma:\n");
+	for(int i=0;i<a;i++){
+		 for(int j=0;j<b;j++){
+		 	suma[i][j]=mat[i][j]+mat2[i][j];
+		 	printf("%i ",suma[i][j]);
+		 }
+		 printf("\n");
+	}
+	
+	printf("\nResta\n");
+	for(int i=0;i<a;i++){
+		for(int j=0;j<a;j++){
+			if(i==j){
+				resta[i][j]=mat[i][j]-mat2[i][j];
+			}else{
+				resta[i][j]=mat[i][j]+mat2[i][j];
+			}
+			printf(" %i",resta[i][j]);
+		}
+		printf("\n");
+		
+	}
+	
+	
+}
